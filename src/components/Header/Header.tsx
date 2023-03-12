dc6a13b45d35af40b7e54f4f87ab8c12a0852db1
Anthony Shellman <atshellman@gmail.com>
2023-03-12 15:26:40 -0600

    Add containers for banner and link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
 import "./Header.css"
 
 const Header: FC = () => {
   return (
-    <div>
-      <h1>IdeaBoxTS</h1>
-    </div>
+    <header className="header-container">
+      <div className="banner-container">
+        <h1 className="banner">IdeaBoxTS</h1>
+      </div>
+      <div className="link-container">
+        <Link to="/archive">Archive View</Link>
+      </div>
+    </header>
   )
 }
 
