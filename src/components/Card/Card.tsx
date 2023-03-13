3254120998a27093a60b346940fd950c21f2cfa7
Anthony Shellman <atshellman@gmail.com>
2023-03-12 19:19:51 -0600

    Add location as props, buttonHandler function, formatting

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,17 +8,30 @@ interface Props {
   deleteIdea: (id: number) => void
   toArchive?: (id: number) => void
   toRestore?: (id: number) => void
+  deleteArchivedIdea?: (id: number) => void
+  location: string
 }
 
-const Card: FC<Props> = ({id, title, description, deleteIdea, toArchive, toRestore }) => {
+const Card: FC<Props> = ({id, title, description, deleteIdea, toArchive,
+                          toRestore, deleteArchivedIdea, location }) => {
+
+  const buttonHandler = (location:string, loc:string) => {
+    return location === loc ?
+     <>
+      <button onClick={() => toArchive?.(id)}>Archive</button>
+      <button onClick={() => deleteIdea(id)}>Delete</button>
+     </> :
+     <>
+      <button onClick={() => toRestore?.(id)}>Restore</button>
+      <button onClick={() => deleteArchivedIdea?.(id)}>Delete</button>
+     </>
+  }
 
   return (
     <div className="card" key={id}>
       <h2>{title}</h2>
       <p>{description}</p>
-      <button onClick={() => toArchive?.(id)}>Archive</button>
-      <button onClick={() => toRestore?.(id)}>Restore</button>
-      <button onClick={() => deleteIdea(id)}>Delete</button>
+      {buttonHandler(location, '/')}
     </div>
   )
 }
